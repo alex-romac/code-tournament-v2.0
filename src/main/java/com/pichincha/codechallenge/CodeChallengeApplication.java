cd28e4764e7c6767822e00620236d8f1ae6ac199
Miguel Alexander Mejía <mmejiabr@pichincha.com>
2022-09-02 12:09:25 -0500

    Read year from console

--- src/main/java/com/pichincha/codechallenge/CodeChallengeApplication.java
+++ src/main/java/com/pichincha/codechallenge/CodeChallengeApplication.java
@@ -1,13 +1,42 @@
 package com.pichincha.codechallenge;
 
+import com.pichincha.codechallenge.service.InterestTableService;
+import com.pichincha.codechallenge.service.impl.InterestTableServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class CodeChallengeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodeChallengeApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+        InterestTableService interestTableService = new InterestTableServiceImpl();
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        Integer years;
+        Integer paymentsPerYear;
+        Double amount;
+        Double yearlyFee;
+        String source;
+        do {
+            System.out.println("Ingrese la cantidad de años: ");
+            source = reader.readLine();
+        } while (!isNumber(source));
+        years = Integer.parseInt(source);
+        interestTableService.generateInterestTable(12.0, 85000.0, 12, years);
+        SpringApplication.run(CodeChallengeApplication.class, args);
+    }
+
+    public static boolean isNumber(String source) {
+        try {
+            Double.parseDouble(source);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
 }
